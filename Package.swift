008f7ae2f215164f6018b4bd210aac7c58734199
au10tixmobile <94300864+au10tixmobile@users.noreply.github.com>
2024-03-05 19:00:04 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -14,8 +14,6 @@ import Foundation
 let package = Package(
     name: "au10tixnfcpassportkit-ios",
     products: [ 
-        .library(name: "Au10tixCore", targets: ["Au10tixCore"]),
-        .library(name: "Au10tixSourceManager", targets: ["Au10tixSourceManager"]),
         .library(name: "Au10tixNFCPassportKit", targets: ["Au10tixNFCPassportKit"])
     ],
     dependencies: [
@@ -23,16 +21,6 @@ let package = Package(
         .package(url: "https://github.com/krzyzanowskim/OpenSSL.git", .upToNextMinor(from: "3.1.4000"))
     ],
     targets: [
-        .binaryTarget(
-          name: "Au10tixCore",
-          url: "https://github.com/au10tixmobile/Au10tixCore/archive/refs/tags/4.0.0.zip",
-          checksum: "ed5691a5d3bf0e519daa6c0e814c9900d77bd6db00aac68b1931afc87d6683f6"
-        ),
-        .binaryTarget(
-          name: "Au10tixSourceManager",
-          url: "https://github.com/au10tixmobile/Au10tixSourceManager/archive/refs/tags/4.0.0.zip",
-          checksum: "8bc55b8fe260a75fcd1a191e2c0f02faf1c10b805c4cbf7c00ea8eb74c66ec1f"
-        ),
         .binaryTarget(
           name: "Au10tixNFCPassportKit",
           url: "https://github.com/au10tixmobile/Au10tixNFCPassportKit/archive/refs/tags/4.0.0.zip",
